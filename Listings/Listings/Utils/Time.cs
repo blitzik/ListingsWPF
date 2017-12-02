30de8c5ed609d32a36d1d761ca063b13b59cbe0f
Aleš Tichava <ales.tichava@gmail.com>
2017-12-02 08:58:06 +0100

    added hours, minutes and seconds to Listings\Utils\Time class

--- Listings/Listings/Utils/Time.cs
+++ Listings/Listings/Utils/Time.cs
@@ -5,11 +5,35 @@ namespace Listings.Utils
 {
     public class Time
     {
+        private int _hours;
+        public int Hours
+        {
+            get { return _hours; }
+            private set { _hours = value; }
+        }
+
+
+        private int _minutes;
+        public int Minutes
+        {
+            get { return _minutes; }
+            private set { _minutes = value; }
+        }
+
+
         private int _seconds;
         public int Seconds
         {
             get { return _seconds; }
             private set { _seconds = value; }
+        }
+
+
+        private int _totalSeconds;
+        public int TotalSeconds
+        {
+            get { return _totalSeconds; }
+            private set { _totalSeconds = value; }
         }
 
 
@@ -37,7 +61,10 @@ namespace Listings.Utils
 
         public Time()
         {
+            Hours = 0;
+            Minutes = 0;
             Seconds = 0;
+            TotalSeconds = 0;
             Text = "00:00:00";
             HoursAndMinutes = "00:00";
         }
@@ -45,16 +72,17 @@ namespace Listings.Utils
 
         public Time(int seconds)
         {
-            Seconds = seconds;
             Text = Seconds2Time(seconds);
+            ProcessTotalSeconds(seconds);
             HoursAndMinutes = GetHoursAndMinutes(this);
         }
 
 
         public Time(string time)
         {
-            Seconds = Time2Seconds(time);
-            Text = Seconds2Time(Seconds);
+            TotalSeconds = Time2Seconds(time);
+            ProcessTotalSeconds(TotalSeconds);
+            Text = Seconds2Time(TotalSeconds);
             HoursAndMinutes = GetHoursAndMinutes(this);
         }
 
@@ -67,7 +95,7 @@ namespace Listings.Utils
 
         public Time GetNegative()
         {
-            return new Time(Seconds * -1);
+            return new Time(TotalSeconds * -1);
         }
 
 
@@ -99,51 +127,60 @@ namespace Listings.Utils
         }
 
 
+        private void ProcessTotalSeconds(int totalSeconds)
+        {
+            TotalSeconds = totalSeconds;
+            Hours = totalSeconds / 3600;
+            Minutes = (totalSeconds % 3600) / 60;
+            Seconds = (totalSeconds % 3600) % 60;
+        }
+
+
         public static Time operator +(Time a, Time b)
         {
-            return new Time(a.Seconds + b.Seconds);
+            return new Time(a.TotalSeconds + b.TotalSeconds);
         }
 
 
         public static Time operator -(Time a, Time b)
         {
-            return new Time(a.Seconds - b.Seconds);
+            return new Time(a.TotalSeconds - b.TotalSeconds);
         }
 
 
         public static bool operator >(Time a, Time b)
         {
-            return a.Seconds > b.Seconds;
+            return a.TotalSeconds > b.TotalSeconds;
         }
 
 
         public static bool operator <(Time a, Time b)
         {
-            return a.Seconds < b.Seconds;
+            return a.TotalSeconds < b.TotalSeconds;
         }
 
 
         public static bool operator <=(Time a, Time b)
         {
-            return a.Seconds <= b.Seconds;
+            return a.TotalSeconds <= b.TotalSeconds;
         }
 
 
         public static bool operator >=(Time a, Time b)
         {
-            return a.Seconds >= b.Seconds;
+            return a.TotalSeconds >= b.TotalSeconds;
         }
 
 
         public static bool operator ==(Time a, Time b)
         {
-            return a.Seconds == b.Seconds;
+            return a.TotalSeconds == b.TotalSeconds;
         }
 
 
         public static bool operator !=(Time a, Time b)
         {
-            return a.Seconds != b.Seconds;
+            return a.TotalSeconds != b.TotalSeconds;
         }
 
 
