1bbd1ce1d037e8770427436ebf1127ae3204538f
Aleš Tichava <ales.tichava@gmail.com>
2018-03-25 11:13:53 +0200

    refactoring, versions are not autogenerated

--- Listings/Evidoo/Properties/SharedAssemblyInfo.cs
+++ Listings/Evidoo/Properties/SharedAssemblyInfo.cs
@@ -4,5 +4,26 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+/*
+ Version information for an assembly consists of the following four values:
+
+ Major Version
+ Minor Version
+ Build Number
+ Revision 
+
+ major = major version. (Kind of big changes, maybe even with UI refresh).
+ minor = as medium set of changes. (maybe new internal processes or engines' refactoring).
+
+ As for build and revision:
+
+ 0 - Means its alpha stage.
+ 1 - Beta.
+ 2 - Release candidate.
+ 3 - Production.
+
+ So, if your app its on 3.2.1.0. You know you're at the alpha stage of the 3.2 version. And so on. 
+*/
+
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
